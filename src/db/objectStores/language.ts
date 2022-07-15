21ce7fbaaea1e0a2045fd121cfeb06c165207993
shoushangdetuolaji <tklijunhao@qq.com>
2022-07-15 15:25:03 +0900

    feat: 添加indexeddb仓库

---
+++ src/db/objectStores/language.ts
@@ -0,0 +1,12 @@
+import TypeObjectStore from '../type'
+
+const webLang: TypeObjectStore = {
+  keyPath: 'id',
+  indexs: ['name']
+}
+
+const languageObjectStore = {
+  language: webLang
+}
+
+export default languageObjectStore
---
+++ src/db/type.ts
@@ -0,0 +1,6 @@
+type TypeObjectStore = {
+  keyPath: string,
+  index?: Array<string>
+} // 定义属性类型
+
+export default TypeObjectStore
