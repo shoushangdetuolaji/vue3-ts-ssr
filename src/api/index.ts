93e62e89c3eb47604c7c5be97a37f47b12155307
shoushangdetuolaji <tklijunhao@qq.com>
2022-07-21 15:40:22 +0900

    feat: http定义一个类函数

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@
  * http://110.42.184.111/api/room/room/getRoomList?pageNo=1&pageSize=6&cityCode=cd
  */
 
-import http from '../utils/http'
+import { http } from '../utils/http'
 
 export function fetchRoomList() {
   return http.httpRequestGet('http://110.42.184.111/api/room/room/getRoomList?pageNo=1&pageSize=6&cityCode=cd', {})
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -47,41 +47,51 @@
 
 // export const http = new Http()
 
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 const defaultConfig = {
   timeout: 5000,
   baseUrl: ''
 }
 
-const axiosInstance = axios.create(defaultConfig)
+class Http {
+  constructor() {
+    this.httpInterceptorsRequest()
+    this.httpInterceptorsResponse()
+  }
 
-// 请求拦截
-axiosInstance.interceptors.request.use(config => {
-  return config
-}, err => {
-  return Promise.reject(err)
-})
+  private static axiosInstance = axios.create(defaultConfig)
 
-// 响应拦截
-axiosInstance.interceptors.response.use(config => {
-  return config
-}, err => {
-  return Promise.reject(err)
-})
+  // 请求拦截
+  private httpInterceptorsRequest() {
+    Http.axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
+      return config
+    }, err => {
+      return Promise.reject(err)
+    })
+  }
 
-// 封装请求
-// get
-function httpRequestGet(url, params) {
-  return axiosInstance.get(url, params).then(res => res.data).catch()
+  // 响应拦截
+  private httpInterceptorsResponse() {
+    Http.axiosInstance.interceptors.response.use((response: AxiosResponse) => {
+      return response
+    }, err => {
+      return Promise.reject(err)
+    })
+  }
+
+  /**
+   * 封装请求
+   */
+  // get
+  public httpRequestGet<T>(url: string, params: AxiosRequestConfig): Promise<T> {
+    return Http.axiosInstance.get(url, { params }).then(res => res.data).catch()
+  }
+
+  // post
+  public httpRequestPost<T>(url: string, params: AxiosRequestConfig): Promise<T> {
+    return Http.axiosInstance.post(url, params).then(res => res.data).catch()
+  }
 }
 
-// post
-function httpRequestPost(url, params) {
-  return axiosInstance.post(url, params).then(res => res.data).catch()
-}
-
-export default {
-  httpRequestGet,
-  httpRequestPost
-}
+export const http = new Http()
