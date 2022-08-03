9ae7f1ea5083c7a600cbd46d126ae4518cacd72f
shoushangdetuolaji <tklijunhao@qq.com>
2022-08-03 18:05:47 +0900

    docs: 客户端首页搭建流程

---
+++ doc/客户端实现Hydrate.md
@@ -0,0 +1,3 @@
+## 客户端实现Hydrate
+- 首页列表布局
+- SSR改造：asyncData、hydrate、ssr-manifest.json
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,65 +1,24 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { h, getCurrentInstance } from 'vue'
-import { fetchRoomList, fetchElephant } from '../../api/index'
-import { useI18n } from 'vue-i18n'
-import { useStore } from '@/store'
-
-const { t } = useI18n()
-console.log(useI18n)
-const router = useRouter()
-const store = useStore()
-
-const { proxy }: any = getCurrentInstance() // 用proxy来代替this
-
-proxy.$message({
-  message: h('p', null, [
-    h('span', null, 'Message can be '),
-    h('i', { style: 'color: teal' }, 'VNode')
-  ])
-})
-
-// 真实接口
-function getRoomList() {
-  fetchRoomList().then(res => {
-    console.log('fetchRoomList', res)
-  })
-}
-getRoomList()
-
-// Mock接口
-function getElephant() {
-  fetchElephant().then(res => {
-    console.log('fetchElephant', res)
-  })
-}
-getElephant()
-
-function setMutataion() {
-  store.commit('setCount', 2)
-}
-
-function setAction() {
-  store.dispatch('fetchCount', 10)
-}
 
 </script>
 
 <template>
-  <div class="home">
-    {{ t('message.home')}}
-    <el-button @click="setMutataion">mutataion</el-button>
-     <el-button  @click="setAction">action</el-button>
-    <button @click="() => router.push({ path: '/mine', query: {id: 1}})">跳转个人中心</button>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <div class="text">十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打十大阿三大苏打</div>
-    <el-date-picker
-      type="week"
-      format="[Week] ww"
-      placeholder="Pick a week"
-    />
+<div class="home-page">
+  <!-- banner -->
+  <div class="banner"></div>
+  <!-- 房屋列表 -->
+  <div class="main-wapper">
+    <h2 class="title">主标题</h2>
+    <p class="sub-title">副标题</p>
+    <div class="home-list">
+      <div class="item" v-for="i in 6" :key="i">
+        <img src="../../assets/images/login/bg.png" alt="">
+        <p class="title">房屋标题</p>
+        <p class="price">134</p>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <style lang="scss">
