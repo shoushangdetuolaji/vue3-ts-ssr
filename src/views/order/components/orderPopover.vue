18f4dd5be7e5fd4e038199084fa66fdf4e7fd47b
shoushangdetuolaji <tklijunhao@qq.com>
2022-08-07 14:14:40 +0800

    feat: 实现订单中心异步组件的数据填充

--- src/views/order/components/orderPopover.vue
+++ src/views/order/components/orderPopover.vue
@@ -1,29 +1,88 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const count = ref(0)
+import { fetchOrderApi } from '@/api/order'
+import { getCurrentInstance, reactive } from 'vue'
 
-function fetchApi() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      count.value = 6
-      resolve(true)
-    }, 3000)
+// const count = ref(0)
+// function fetchApi() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       count.value = 6
+//       resolve(true)
+//     }, 3000)
+//   })
+// }
+// await fetchApi()
+let orderData = reactive<Array<any>>([])
+const { proxy }: any = getCurrentInstance()
+function fetchOrder() {
+  return fetchOrderApi().then((res) => {
+    console.log(res)
+    const { result, success, message } = res
+    if (success) {
+      console.log('成功')
+      orderData = result
+    } else {
+      proxy.$message.error(message)
+    }
   })
 }
-await fetchApi()
+await fetchOrder()
 </script>
 
 <template>
   <ul>
-    <li v-for="i in 6" :key="i">
-      <img src="https://oss.dalieyingcai.com/static/temp/cdn-airbnb/images/1/1.jpg" alt="">
-      <div class="item">
-        <p class="title">北京四合院</p>
-        <p class="info">234/晚 * 1个人</p>
+    <li v-for="(item, index) in orderData" :key="index">
+      <img :src="item.pictureUrl" alt="">
+      <div class="mess">
+        <p class="title">{{ item.title }}</p>
+        <p class="info">{{ item.price }}/晚 * {{ item.personNumber }}个人</p>
       </div>
     </li>
   </ul>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+ul {
+  @include placeholder-order;
+}
+li {
+  @include flex-layout(row, space-between, center);
+  border-bottom: 1px solid #eee;
+  padding: 10px 0;
+  &:last-child {
+    border-bottom: none;
+  }
+  img {
+    width: 65px;
+    height: 45px;
+    border-radius: 4px;
+    margin-right: 5px;
+    object-fit: cover;
+  }
+  .mess {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    margin-left: 5px;
+    width: 100px;
+    overflow: hidden;
+    p {
+      line-height: 16px;
+      font-weight: normal;
+      margin: 5px 0;
+      max-width: 100px;
+    }
+    .title {
+      font-weight: bold;
+      color: #333;
+      font-size: 14px;
+      display: inline-block;
+      @include line-text-overflow;
+    }
+    .info {
+      color: #666;
+      font-size: 12px;
+    }
+  }
+}
 </style>
